494d105d47752662c968775ea3e14a5fb14107b4
Andi Barron <andibarron7@gmail.com>
2024-07-31 22:06:58 -0700

    Added resume download

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,35 +1,24 @@
 import React from 'react';
 
-function Contact({ onButtonClick }) {
+function Contact() {
   return (
     <div>
       <h2>Contact</h2>
       <div className="social-links">
-        <a href="https://github.com/andib7" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-github"></i> GitHub
-        </a>
-        <a href="https://linkedin.com/in/abarron7" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-linkedin"></i> LinkedIn
-        </a>
-        <a href="https://andib7.itch.io/" target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-gamepad"></i> Itch.io
-        </a>
-        <a href="https://editor.p5js.org/andibarron7/sketches" target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-laptop-code"></i> p5.js
-        </a>
-        <a href="https://leetcode.com/u/andibarron7/" target="_blank" rel="noopener noreferrer">
-          <i className="fas fa-code"></i> Leetcode
-        </a>
+        <a href="https://github.com/andib7" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> GitHub</a>
+        <a href="https://linkedin.com/in/abarron7" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i> LinkedIn</a>
+        <a href="https://andib7.itch.io/" target="_blank" rel="noopener noreferrer"><i className="fas fa-gamepad"></i> Itch.io</a>
+        <a href="https://editor.p5js.org/andibarron7/sketches" target="_blank" rel="noopener noreferrer"><i className="fas fa-laptop-code"></i> p5.js</a>
+        <a href="https://leetcode.com/u/andibarron7/" target="_blank" rel="noopener noreferrer"><i className="fas fa-code"></i> Leetcode</a>
       </div>
       <p>You can reach me at:</p>
       <ul className="contact-info">
         <li>Email: <a href="mailto:andibarron7@gmail.com">andibarron7@gmail.com</a></li>
         <li>Phone: <a href="tel:+12096664175">+1 (209) 666-4175</a></li>
       </ul>
-      {/* Add a close button that triggers onButtonClick */}
-      <button className="close-button" onClick={onButtonClick}>
-        Close
-      </button>
+      <a href="path/to/resume.pdf" download className="button download-button">
+        Download Resume
+      </a>
     </div>
   );
 }
